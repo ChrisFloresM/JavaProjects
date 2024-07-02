24c4f513631d02ebc0126e28998aba9b452f3cca
Christian <christian.mig.flmo@gmail.com>
2024-07-02 13:45:41 -0600

    Finishing refactoring with enums

--- CoffeMachine/src/com/cfloresh/coffemachine/CoffeeMaker.java
+++ CoffeMachine/src/com/cfloresh/coffemachine/CoffeeMaker.java
@@ -18,18 +18,163 @@ public class CoffeeMaker {
     /* CoffeeOrder object with information about the order */
     private CoffeeOrder coffeeOrder;
 
-    /* enum of states */
+    /* enum of states - each state and its defined actions */
     enum States {
-        START,
-        BUY,
-        FILL_WATER,
-        FILL_MILK,
-        FILL_COFFEE,
-        FILL_CUPS,
-        REMAINING,
-        TAKE,
-        EXIT
-/*        abstract void stateAction();*/
+        START {
+            void stateAction(String input, CoffeeMaker instance) {
+                if (!instance.getReadInput()) {
+                    System.out.println("Write action (buy, fill, take, remaining, exit): ");
+                    instance.setReadInput(true);
+                } else {
+                    instance.setReadInput(false);
+                    if (!instance.validateStartInput(input)) {
+                        System.out.println("Invalid command!\n");
+                        return;
+                    }
+
+                    if (input.equalsIgnoreCase("fill")) {
+                        instance.setState(States.FILL_WATER);
+                    } else {
+                        instance.setState(States.valueOf(input.toUpperCase()));
+                    }
+
+                }
+            }
+        },
+
+        BUY {
+            void stateAction(String input, CoffeeMaker instance) {
+                if(!instance.getReadInput()) {
+                    System.out.println("\nwhat do you want to buy? 1 - espresso, 2 - latte, 3 - capuccino, back - to main menu: ");
+                    instance.setReadInput(true);
+                } else {
+                    instance.setReadInput(false);
+                    if(!instance.validateBuyInput(input)) {
+                        System.out.println("Invalid command!");
+                        return;
+                    }
+
+                    if (!input.equalsIgnoreCase("back")) {
+                        instance.setCoffeeOrder(new CoffeeOrder(Integer.parseInt(input)));
+                        if (instance.validateOrder()) {
+                            instance.processOrder();
+                        }
+                    }
+
+                    instance.setState(States.START);
+                    System.out.println();
+                }
+            }
+        },
+
+        FILL_WATER {
+            void stateAction(String input, CoffeeMaker instance) {
+                //do nothing
+                if(!instance.getReadInput()) {
+                    System.out.println("\nWrite how many ml of water you want to add:");
+                    instance.setReadInput(true);
+                } else {
+                    instance.setReadInput(false);
+                    if(!instance.validateFillInput(input)) {
+                        System.out.println("Input a numeric value!");
+                        return;
+                    }
+                    instance.increaseWater(Integer.parseInt(input));
+                    instance.setState(States.FILL_MILK);
+                }
+            }
+        },
+
+        FILL_MILK {
+            void stateAction(String input, CoffeeMaker instance) {
+                //do nothing
+                if(!instance.getReadInput()) {
+                    System.out.println("\nWrite how many ml of milk you want to add:");
+                    instance.setReadInput(true);
+                } else {
+                    instance.setReadInput(false);
+                    if(!instance.validateFillInput(input)) {
+                        System.out.println("Input a numeric value!");
+                        return;
+                    }
+                    instance.increaseMilk(Integer.parseInt(input));
+                    instance.setState(States.FILL_COFFEE);
+                }
+            }
+        },
+        FILL_COFFEE {
+            void stateAction(String input, CoffeeMaker instance) {
+                //do nothing
+                if(!instance.getReadInput()) {
+                    System.out.println("\nWrite how many grams of coffee beans you want to add:");
+                    instance.setReadInput(true);
+                } else {
+                    instance.setReadInput(false);
+                    if(!instance.validateFillInput(input)) {
+                        System.out.println("Input a numeric value!");
+                        return;
+                    }
+                    instance.increaseCoffee(Integer.parseInt(input));
+                    instance.setState(States.FILL_CUPS);
+                }
+            }
+        },
+
+        FILL_CUPS {
+            void stateAction(String input, CoffeeMaker instance) {
+                //do nothing
+                if(!instance.getReadInput()) {
+                    System.out.println("\nWrite how many disposable cups you want to add:");
+                    instance.setReadInput(true);
+                } else {
+                    instance.setReadInput(false);
+                    if(!instance.validateFillInput(input)) {
+                        System.out.println("Input a numeric value!");
+                        return;
+                    }
+                    instance.increaseCups(Integer.parseInt(input));
+                    instance.setState(States.START);
+                }
+            }
+        },
+        REMAINING {
+            void stateAction(String input, CoffeeMaker instance) {
+                //do nothing
+                String status = String.format("""
+                
+                The coffee machine has:
+                %d ml of water
+                %d ml of milk
+                %d g of coffee beans
+                %d disposable cups
+                $%d of money
+                """, instance.getTotalWater(), instance.getTotalMilk(), instance.getTotalCoffe(),
+                        instance.getTotalCups(), instance.getTotalMoney());
+
+                System.out.print(status);
+                instance.setState(States.START);
+                System.out.println();
+            }
+        },
+
+        TAKE {
+            void stateAction(String input, CoffeeMaker instance) {
+                //do nothing
+                System.out.printf("I gave you $%d\n", instance.getTotalMoney());
+                instance.setTotalMoney(0);
+                instance.setState(States.START);
+                System.out.println();
+            }
+        },
+
+        EXIT {
+            void stateAction(String input, CoffeeMaker instance) {
+                //do nothing
+                instance.setExit(true);
+            }
+        };
+
+        abstract void stateAction(String input, CoffeeMaker instance);
     }
 
     /* Constructor to initialize fields and state */
@@ -45,107 +190,110 @@ public class CoffeeMaker {
         state = States.START;
     }
 
+    /* state machine method which will be called on each iteration while the machine is ON */
     public void stateMachine(){
-        switch (this.state) {
-            case START -> stateStart();
-            case BUY -> stateBuy();
-            case FILL_WATER -> stateFillWater();
-            case FILL_MILK -> stateFillMilk();
-            case FILL_COFFEE -> stateFillCoffee();
-            case FILL_CUPS -> stateFillCups();
-            case REMAINING -> stateRemaining();
-            case TAKE -> stateTake();
-            case EXIT -> stateExit();
-        }
+        this.state.stateAction(this.userInput, this);
     }
 
     public void setUserInput(String input) {
         this.userInput = input;
     }
 
+    /* Getters and setters - Exit flag*/
     public boolean getExit() {
         return this.exit;
     }
 
+    public void setExit(boolean state) {
+        this.exit = state;
+    }
+
+    /* Getter and setter - readInput flag */
     public boolean getReadInput() {
         return this.readInput;
     }
 
-    private void stateExit() {
-        this.exit = true;
+    public void setReadInput(boolean readInput) {
+        this.readInput = readInput;
     }
 
-    private void stateStart() {
-        if (!this.readInput) {
-            System.out.println("Write action (buy, fill, take, remaining, exit): ");
-            this.readInput = true;
-        } else {
-            this.readInput = false;
-            if (!validateStartInput()) {
-                System.out.println("Invalid command!\n");
-                return;
-            }
-
-            if (this.userInput.equalsIgnoreCase("fill")) {
-                this.state = States.FILL_WATER;
-            } else {
-                this.state = States.valueOf(this.userInput.toUpperCase());
-            }
-
-        }
+    /* Setter for the CoffeOrder object */
+    public void setCoffeeOrder(CoffeeOrder coffeeOrder) {
+        this.coffeeOrder = coffeeOrder;
     }
 
-    private boolean validateStartInput() {
+    /* Setter for the state flag */
+    public void setState(States state) {
+        this.state = state;
+    }
 
-        boolean validBuyCmd = this.userInput.equalsIgnoreCase("buy");
-        boolean validFillCmd = this.userInput.equalsIgnoreCase("fill");
-        boolean validTakeCmd = this.userInput.equalsIgnoreCase("take");
-        boolean validRemCmd = this.userInput.equalsIgnoreCase("remaining");
-        boolean validExitCmd = this.userInput.equalsIgnoreCase("exit");
+    /* Setters to increase each ingredient by a given amount */
+    public void increaseWater(int value) {
+        this.totalWater += value;
+    }
+
+    public void increaseMilk(int value) {
+        this.totalMilk += value;
+    }
+
+    public void increaseCoffee(int value) {
+        this.totalCoffe += value;
+    }
+
+    public void increaseCups(int value) {
+        this.totalCups += value;
+    }
+
+    /* Getter for each ingredient */
+    public int getTotalWater() {
+        return totalWater;
+    }
+
+    public int getTotalMilk() {
+        return totalMilk;
+    }
+
+    public int getTotalCoffe() {
+        return totalCoffe;
+    }
+
+    public int getTotalCups() {
+        return totalCups;
+    }
+
+    public int getTotalMoney() {
+        return totalMoney;
+    }
+
+    /* Setter to reset the money when taken from the user */
+    public void setTotalMoney(int totalMoney) {
+        this.totalMoney = totalMoney;
+    }
+
+    /* Method used to validate the input from the user on the Start state */
+    public boolean validateStartInput(String input) {
+        boolean validBuyCmd = input.equalsIgnoreCase("buy");
+        boolean validFillCmd = input.equalsIgnoreCase("fill");
+        boolean validTakeCmd = input.equalsIgnoreCase("take");
+        boolean validRemCmd = input.equalsIgnoreCase("remaining");
+        boolean validExitCmd = input.equalsIgnoreCase("exit");
 
         return validBuyCmd || validFillCmd || validTakeCmd || validRemCmd || validExitCmd;
 
     }
 
-    private void stateBuy(){
-        if(!this.readInput) {
-            System.out.println("\nwhat do you want to buy? 1 - espresso, 2 - latte, 3 - capuccino, back - to main menu: ");
-            this.readInput = true;
-        } else {
-            this.readInput = false;
-
-            if(!validateBuyInput()) {
-                System.out.println("Invalid command!");
-                return;
-            }
-
-            String coffeType = this.userInput;
-            if (!coffeType.equalsIgnoreCase("back")) {
-                coffeeOrder = new CoffeeOrder(Integer.parseInt(coffeType));
-                if (validateOrder()) {
-                    totalWater -= coffeeOrder.getOrderWater();
-                    totalMilk -= coffeeOrder.getOrderMilk();
-                    totalCoffe -= coffeeOrder.getOrderCoffe();
-                    totalCups--;
-                    totalMoney += coffeeOrder.getOrderPrice();
-                }
-            }
-
-            this.state = States.START;
-            System.out.println();
-        }
-    }
-
-    private boolean validateBuyInput() {
-        boolean validEspressCmd = this.userInput.equalsIgnoreCase("1");
-        boolean validLatteCmd = this.userInput.equalsIgnoreCase("2");
-        boolean validCapCmd = this.userInput.equalsIgnoreCase("3");
-        boolean validBackCmd = this.userInput.equalsIgnoreCase("back");
+    /* Method to validate the input from the user on the Buy state */
+    public boolean validateBuyInput(String input) {
+        boolean validEspressCmd = input.equalsIgnoreCase("1");
+        boolean validLatteCmd = input.equalsIgnoreCase("2");
+        boolean validCapCmd = input.equalsIgnoreCase("3");
+        boolean validBackCmd = input.equalsIgnoreCase("back");
 
         return validEspressCmd || validLatteCmd || validCapCmd || validBackCmd;
     }
 
-    private boolean validateOrder() {
+    /* Method to validate that an order can be performed */
+    public boolean validateOrder() {
         boolean result = true;
 
         int orderWater = coffeeOrder.getOrderWater();
@@ -178,23 +326,18 @@ public class CoffeeMaker {
         return result;
     }
 
-    private void stateFillWater() {
-        if(!this.readInput) {
-            System.out.println("\nWrite how many ml of water you want to add:");
-            this.readInput = true;
-        } else {
-            this.readInput = false;
-            if(!validateFillInput()) {
-                System.out.println("Input a numeric value!");
-                return;
-            }
-            this.totalWater += Integer.parseInt(this.userInput);
-            this.state = States.FILL_MILK;
-        }
+    /* Method to process a coffe order by decreasing the amount of ingredients */
+    public void processOrder(){
+        totalWater -= coffeeOrder.getOrderWater();
+        totalMilk -= coffeeOrder.getOrderMilk();
+        totalCoffe -= coffeeOrder.getOrderCoffe();
+        totalCups--;
+        totalMoney += coffeeOrder.getOrderPrice();
     }
 
-    private boolean validateFillInput() {
-        for (Character c : this.userInput.toCharArray()) {
+    /* Method to validate the user input on any of the Fill states */
+    public boolean validateFillInput(String input) {
+        for (Character c : input.toCharArray()) {
             if (!Character.isDigit(c)) {
                 return false;
             }
@@ -202,74 +345,6 @@ public class CoffeeMaker {
         return true;
     }
 
-    private void stateFillMilk() {
-        if(!this.readInput) {
-            System.out.println("\nWrite how many ml of milk you want to add:");
-            this.readInput = true;
-        } else {
-            this.readInput = false;
-            if(!validateFillInput()) {
-                System.out.println("Input a numeric value!");
-                return;
-            }
-            this.totalMilk += Integer.parseInt(this.userInput);
-            this.state = States.FILL_COFFEE;
-        }
-    }
-
-    private void stateFillCoffee() {
-        if(!this.readInput) {
-            System.out.println("\nWrite how many grams of coffee you want to add:");
-            this.readInput = true;
-        } else {
-            this.readInput = false;
-            if(!validateFillInput()) {
-                System.out.println("Input a numeric value!");
-                return;
-            }
-            this.totalCoffe += Integer.parseInt(this.userInput);
-            this.state = States.FILL_CUPS;
-        }
-    }
-
-    private void stateFillCups() {
-        if(!this.readInput) {
-            System.out.println("\nWrite how many disposable cups you want to add:");
-            this.readInput = true;
-        } else {
-            this.readInput = false;
-            if(!validateFillInput()) {
-                System.out.println("Input a numeric value!");
-                return;
-            }
-            this.totalCups += Integer.parseInt(this.userInput);
-            this.state = States.START;
-            System.out.println();
-        }
-    }
-
-    private void stateRemaining() {
-        String status = String.format("""
-                
-                The coffee machine has:
-                %d ml of water
-                %d ml of milk
-                %d g of coffee beans
-                %d disposable cups
-                $%d of money
-                """, totalWater, totalMilk, totalCoffe, totalCups, totalMoney);
-
-        System.out.print(status);
-        this.state = States.START;
-        System.out.println();
-    }
-
-    private void stateTake() {
-        System.out.printf("I gave you $%d\n", totalMoney);
-        totalMoney = 0;
-        this.state = States.START;
-        System.out.println();
-    }
 }
 
 
